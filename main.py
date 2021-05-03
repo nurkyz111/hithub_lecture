c7706917a908d875468a82ddc2dfc2ebd3203afb
nurkyz111 <bektawovanurkyz@gmail.com>
2021-05-03 20:58:08 +0600

    cycle

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print('Nurkyz')
 print('Ogogo')
 print('science')
 print('forms')
+
+lst = ['Nurkyz', 'Adliet', 'Abai', 'Siymyk', 'Ulan', 'Adilet', 'Richard', 'Insan', 'Azim']
+for i in lst:
+    print(i[0])
